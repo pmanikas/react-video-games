b68efa16adca2d63e65c68b8952662dfaad5bb75
pmanikas <pmanikas.social@gmail.com>
2021-03-17 13:48:03 +0200

    Add spinner on page load

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import { loadGames, loadDetail } from "./../../store/actions/gamesAction";
 
 import GameDetails from "../../components/GameDetails/GameDetails";
 import GamesList from "./../../components/GamesList/GamesList";
+import Spinner from "./../../components/Spinner/Spinner";
 
 import styles from "./Home.module.scss";
 
@@ -36,18 +37,23 @@ const Home = () => {
 
   return (
     <AnimateSharedLayout type="crossfade">
-      <div className={styles.container}>
-      {searched && !!searched.length && (
-        <GamesList games={searched} title={`Search Results`} />
+      {areGamesLoading && (
+        <div className={styles.pageLoader}>
+          <Spinner className={styles.spinner} />
+        </div>
       )}
-      <GamesList games={upcoming} title={`Upcoming Games`} />
-      <GamesList games={popular} title={`Popular Games`} />
-      <GamesList games={newGames} title={`New Games`} />
-      <AnimatePresence>
-        {pathId && !isGameLoading && game && game.id && !areGamesLoading && (
-          <GameDetails pathId={pathId} />
+      <div className={styles.container}>
+        {searched && !!searched.length && (
+          <GamesList games={searched} title={`Search Results`} />
         )}
-      </AnimatePresence>
+        <GamesList games={upcoming} title={`Upcoming Games`} />
+        <GamesList games={popular} title={`Popular Games`} />
+        <GamesList games={newGames} title={`New Games`} />
+        <AnimatePresence>
+          {pathId && !isGameLoading && game && game.id && !areGamesLoading && (
+            <GameDetails pathId={pathId} />
+          )}
+        </AnimatePresence>
       </div>
     </AnimateSharedLayout>
   );
