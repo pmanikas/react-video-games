153211499a2820aa72eeabf9524d172ff654686b
pmanikas <pmanikas.social@gmail.com>
2021-03-17 14:24:49 +0200

    Improve search ux

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,32 +3,44 @@ import styles from "./Search.module.scss";
 
 const Search = ({ submitSearch }) => {
   const [textInput, setTextInput] = useState("");
+  const [isFocused, setIsFocused] = useState("");
 
   const changeInputHandler = (e) => {
     setTextInput(e.target.value);
   };
   const submitSearchHandler = (e) => {
     e.preventDefault();
-    submitSearch(textInput);
-    setTextInput("");
+    if (textInput) {
+      submitSearch(textInput);
+      setTextInput("");
+    }
+  };
+
+  const setFocusedHandler = (state) => {
+    setIsFocused(state);
   };
 
   return (
-    <form className={styles.searchForm}>
-      <input
-        className={styles.searchInput}
-        value={textInput}
-        onChange={changeInputHandler}
-        type="text"
-        placeholder="Search for Game Titles..."
-      />
-      <button
-        className={styles.searchButton}
-        onClick={submitSearchHandler}
-        type="submit"
-      >
-        Search
-      </button>
+    <form className={`${styles.searchForm} ${isFocused ? styles.focus : ""}`}>
+      <div className={styles.searchContainer}>
+        <input
+          className={styles.searchInput}
+          value={textInput}
+          onChange={changeInputHandler}
+          type="text"
+          autoFocus
+          onFocus={() => setFocusedHandler(true)}
+          onBlur={() => setFocusedHandler(false)}
+          placeholder="Search for Game Titles..."
+        />
+        <button
+          className={styles.searchButton}
+          onClick={submitSearchHandler}
+          type="submit"
+        >
+          Search
+        </button>
+      </div>
     </form>
   );
 };
