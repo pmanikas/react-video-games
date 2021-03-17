f387f6754a279975fa6009a1f1c9152ee2e2870c
pmanikas <pmanikas.social@gmail.com>
2021-03-17 02:54:38 +0200

    Clean code and add time utils

--- src/services/games.service.js
+++ src/services/games.service.js
@@ -1,61 +1,36 @@
 import { get } from "./api.service";
+import { getCurrentMonth, getCurrentDay } from "./../utils/timeFormat.utils";
 
 const BASE_URL = "https://api.rawg.io/api/";
 const API_KEY = process.env.REACT_APP_API_KEY || "";
 
-const getCurrentMonth = () => {
-  const month = new Date().getMonth() + 1;
-  if (month < 10) {
-    return `0${month}`;
-  } else {
-    return month;
-  }
-};
-const getCurrentDay = () => {
-  const day = new Date().getDate();
-  if (day < 10) {
-    return `0${day}`;
-  } else {
-    return day;
-  }
-};
-
 const currentYear = new Date().getFullYear();
-const currentMonth = getCurrentMonth();
-const currentDay = getCurrentDay();
+const currentMonth = getCurrentMonth(new Date());
+const currentDay = getCurrentDay(new Date());
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
-export const popularGamesURL = `${BASE_URL}games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-export const upcomingGamesURL = `${BASE_URL}games?key=${API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-export const newGamesURL = `${BASE_URL}games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popularGamesURL = `${BASE_URL}games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcomingGamesURL = `${BASE_URL}games?key=${API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGamesURL = `${BASE_URL}games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
-export const GameDetailsURL = (game_id) =>
-  `${BASE_URL}games/${game_id}?key=${API_KEY}`;
-export const gameScreenshotURL = (game_id) =>
-  `${BASE_URL}games/${game_id}/screenshots?key=${API_KEY}`;
-export const searchGameURL = (query) =>
+const gameDetailsURL = (id) =>
+  `${BASE_URL}games/${id}?key=${API_KEY}`;
+const gameScreenshotURL = (id) =>
+  `${BASE_URL}games/${id}/screenshots?key=${API_KEY}`;
+
+const searchGameURL = (query) =>
   `${BASE_URL}games?key=${API_KEY}&search=${query}&page_size=9`;
 
-const getPopularGames = () => {
-  return get(popularGamesURL);
-};
-const getUpcomingGames = () => {
-  return get(upcomingGamesURL);
-};
-const getNewGames = () => {
-  return get(newGamesURL);
-};
-const getSingleGame = (game_id) => {
-  return get(GameDetailsURL(game_id));
-};
-const getSingleGameScreenshot = (game_id) => {
-  return get(gameScreenshotURL(game_id));
-};
-const searchGames = (query) => {
-  return get(searchGameURL(query));
-};
+const getPopularGames = () => get(popularGamesURL);
+const getUpcomingGames = () => get(upcomingGamesURL);
+const getNewGames = () => get(newGamesURL);
+
+const getSingleGame = (id) => get(gameDetailsURL(id));
+const getSingleGameScreenshot = (id) => get(gameScreenshotURL(id));
+
+const searchGames = (query) => get(searchGameURL(query));
 
 export {
   getPopularGames,
--- src/utils/timeFormat.utils.js
+++ src/utils/timeFormat.utils.js
@@ -1,3 +1,22 @@
-export const covertMsToTime = (ms) => {
+const covertMsToTime = (ms) => {
   return `${Math.floor(ms / 60)}:${("0" + Math.floor(ms % 60)).slice(-2)}`;
 };
+
+const getCurrentMonth = (date) => {
+  const month = date.getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDay = (date) => {
+  const day = date.getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+export { covertMsToTime, getCurrentMonth, getCurrentDay };
